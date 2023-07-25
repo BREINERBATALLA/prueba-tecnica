b4e6f71054b38b9f5af5208e87f4c56d7406c349
BREINER BATALLA <breiner.batalla@correounivalle.edu.co>
2023-07-24 21:21:00 -0500

    [ADD] openApi configuration

---
+++ src/main/java/com/nexsys/prueba/infraestructure/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.nexsys.prueba.infraestructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Breiner Batalla",
+                        email = "breiner.batalla@correounivalle.edu.co"
+                ),
+                description = "OpenApi documentation for nexsys tecnhical interview",
+                title = "OpenApi specification - Consuming external services",
+                version = "1.0",
+                termsOfService = "Tearms of service"
+
+        ),
+        servers = {
+                @Server(
+                        description = "LOCAL ENVIRONMENT",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+public class OpenApiConfig {
+}
